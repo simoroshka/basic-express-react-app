747a3a0ce4cad56c564f8828d9ccff306cde7d62
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2019-01-08 13:53:17 +0200

    Add CRA and serve its static files with the same express server

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('/api', (req, res) => res.send('Hello World!'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
