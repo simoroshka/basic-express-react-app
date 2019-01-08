9a7040b278b40a7e90613e4e96ef4ce51e437a81
Anna Kruglaia <anna.kruglaia@nemesys.fi>
2019-01-08 15:12:14 +0200

    Set up development with hmr

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  state = { message: 'Loading...' };
+  componentDidMount = async () => {
+    const res = await axios.get('/api');
+    this.setState({ message: res.data });
+  };
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <p>{this.state.message}</p>
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 4000;
 
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-app.get('/api', (req, res) => res.send('Hello World!'));
+app.get('/api', (req, res) => res.send('Hello World!!'));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname + '/client/build/index.html'));
